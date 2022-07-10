131cfb31234965285a419f4ad667cfdfaf67613d
webfaisalbd <web.faisal.bd@gmail.com>
2022-07-10 22:40:47 +0600

    odd numbers done

--- 7.js
+++ 7.js
@@ -3,5 +3,8 @@ const numbers = [1, 2, 3, 8, 9, 12, 16];
 const even = numbers.filter((item) => {
     return item % 2 == 0;
 })
-
+const odd = numbers.filter((item) => {
+    return item % 2 ==! 0;
+})
 console.log("Even numbers: ",even);
+console.log("Odd numbers: ",odd);
